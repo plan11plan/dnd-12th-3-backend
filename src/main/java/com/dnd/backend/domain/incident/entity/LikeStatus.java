26523c42332fe942856d8e3866dd6aa41ed3aa82
plan11plan <tjtfy7028@gmail.com>
2025-02-14 17:56:56 +0900

    feat: 게시글좋아요 상태 추가 (LiKE,UNLIKE)

--- src/main/java/com/dnd/backend/domain/incident/entity/LikeStatus.java
+++ src/main/java/com/dnd/backend/domain/incident/entity/LikeStatus.java
@@ -4,14 +4,6 @@ import lombok.Getter;
 
 @Getter
 public enum LikeStatus {
-	LIKE(1, "Like"),
-	UNLIKE(0, "CancelLike/Not Like");
-
-	private int code;
-	private String msg;
-
-	LikeStatus(int code, String msg) {
-		this.code = code;
-		this.msg = msg;
-	}
+	LIKE,
+	UNLIKE
 }
