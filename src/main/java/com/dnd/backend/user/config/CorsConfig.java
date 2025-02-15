a59b08cc61d15e84fcc5e3302321e4b7b068e2a0
plan11plan <tjtfy7028@gmail.com>
2025-02-15 20:37:38 +0900

    feat: CorsConfig 의존성 추가

---
+++ src/main/java/com/dnd/backend/user/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.dnd.backend.user.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+	@Value("${app.cors.allowed-origins}")
+	private String allowedOrigins;
+
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return new CorsFilter(source);
+	}
+}
